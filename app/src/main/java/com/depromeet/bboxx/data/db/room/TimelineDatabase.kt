c22c9b65060d97e54687f338aa52d801c8ced9a7
JungGeun Lee <jotolo@naver.com>
2021-11-13 01:40:16 +0900

    Keystore 생성 완료
    Keystore 생성 완료

--- app/src/main/java/com/depromeet/bboxx/data/db/room/TimelineDatabase.kt
+++ app/src/main/java/com/depromeet/bboxx/data/db/room/TimelineDatabase.kt
@@ -1,10 +1,10 @@
 package com.depromeet.bboxx.data.db.room
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Timeline::class], version = 1)
-abstract class TimelineDatabase : RoomDatabase() {
-    abstract fun timelineListDao(): TimelineDao
-
-}
+//
+//import androidx.room.Database
+//import androidx.room.RoomDatabase
+//
+//@Database(entities = [Timeline::class], version = 1)
+//abstract class TimelineDatabase : RoomDatabase() {
+//    abstract fun timelineListDao(): TimelineDao
+//
+//}
