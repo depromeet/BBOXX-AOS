1fed8fd2d33530c6240531123f1210f017f29d3b
구해린 <ninesunzero@gmail.com>
2021-11-18 22:12:01 +0900

    DONE - 공유하기 기능 추가 - 토스트 메시지 수정

--- app/src/main/java/com/depromeet/bboxx/presentation/ui/decibel/DecibelResultFragment.kt
+++ app/src/main/java/com/depromeet/bboxx/presentation/ui/decibel/DecibelResultFragment.kt
@@ -3,37 +3,32 @@ package com.depromeet.bboxx.presentation.ui.decibel
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import com.depromeet.bboxx.BuildConfig
 import com.depromeet.bboxx.R
 import com.depromeet.bboxx.databinding.DecibelResultLayoutBinding
 import com.depromeet.bboxx.presentation.ui.MainActivity
 import com.depromeet.bboxx.presentation.ui.feelnote.FeelingNoteSelectFragment
 import com.depromeet.bboxx.presentation.utils.CustomTopView
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.net.Uri
-import android.os.Build
-
-import android.os.Environment
-import android.util.Log
-import android.widget.Toast
-import androidx.core.content.FileProvider
-import com.depromeet.bboxx.BuildConfig
 import java.io.File
-import java.io.FileNotFoundException
 import java.io.FileOutputStream
-import java.lang.String
 import java.text.SimpleDateFormat
 import java.util.*
-import com.kakao.auth.StringSet.file
 
 
-class DecibelResultFragment(val dB: Int) : Fragment() {
+class DecibelResultFragment(private val dB: Int) : Fragment() {
 
     lateinit var mainActivity: MainActivity
     override fun onAttach(context: Context) {
@@ -46,7 +41,7 @@ class DecibelResultFragment(val dB: Int) : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         val binding = DecibelResultLayoutBinding.inflate(inflater, container, false)
 
@@ -55,7 +50,7 @@ class DecibelResultFragment(val dB: Int) : Fragment() {
     }
 
 
-    @SuppressLint("ResourceType", "UseCompatLoadingForDrawables")
+    @SuppressLint("ResourceType", "UseCompatLoadingForDrawables", "SetTextI18n")
     private fun initView(binding: DecibelResultLayoutBinding, container: ViewGroup?) {
 
         binding.clTopView.setRightBtn(object : CustomTopView.OnclickCallback {
@@ -64,7 +59,6 @@ class DecibelResultFragment(val dB: Int) : Fragment() {
             }
         }, R.drawable.ic_close, resources.getString(R.color.main_bg))
         binding.tvResutDb.text = dB.toString() + "dB"
-        //TODO HAERIN 감정 측정도에 맞게 세팅
 
         @SuppressLint("SetTextI18n")
 
@@ -118,22 +112,19 @@ class DecibelResultFragment(val dB: Int) : Fragment() {
             try {
                 val sdf = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA)
                 val fileName = "/" + sdf.format(Date()).toString() + ".jpg"
-                val mPath =
-                    Environment.getExternalStorageDirectory().toString() + fileName
-
+                val mPath = Environment.getExternalStorageDirectory().toString() + fileName
                 val bitmap = getBitmapFromView(container?.rootView)
 
                 // 이미지 파일 생성
                 val imageFile = File(mPath)
                 val outputStream = FileOutputStream(imageFile)
-                bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-                outputStream.flush();
-                outputStream.close();
+                bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
 
-                Toast.makeText(context, "해당 화면이 캡쳐되었습니다 :)", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "이미지 저장 완료:D", Toast.LENGTH_SHORT).show()
 
-                //확인 후 추가
-//                shareJPG(imageFile)
+                shareJPG(imageFile)
 
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -142,43 +133,39 @@ class DecibelResultFragment(val dB: Int) : Fragment() {
 
         }
     }
-//
-//    private fun shareJPG(img : File){
-//
-//        try {
-//            val shareIntent = Intent(Intent.ACTION_SEND)
-//
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-////                val contentUri = Uri.fromFile(img)
-//                shareIntent.type = "image/*";
-//
-//                val contentUri = FileProvider.getUriForFile(mainActivity,
-//                    BuildConfig.APPLICATION_ID + ".provider", img);
-////                val contentUri = FileProvider.getUriForFile(mainActivity, , file)
-//
-//                shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
-//            } else {
-//
-//                shareIntent.type = "image/*";
-//                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(img));
-//
-//            }
-//
-//
-//            mainActivity.startActivity(Intent.createChooser(shareIntent, ""));
-//
-//        }catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//    }
+
+    private fun shareJPG(img: File) {
+
+        try {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                shareIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                shareIntent.type = "image/*"
+
+                val contentUri = FileProvider.getUriForFile(
+                    mainActivity,
+                    BuildConfig.APPLICATION_ID + ".provider", img
+                )
+
+                shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
+            } else {
+                shareIntent.type = "image/*"
+                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(img))
+            }
+
+            mainActivity.startActivity(Intent.createChooser(shareIntent, ""))
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
 
     private fun getBitmapFromView(view: View?): Bitmap? {
         if (view == null) return null
-        var bitmap =
-            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-        var canvas = Canvas(bitmap)
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
         view.draw(canvas)
         return bitmap
     }
